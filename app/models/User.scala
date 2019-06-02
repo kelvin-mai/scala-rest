5944e6245bbe2ec0ec9ca3db6e1e0f39342884c1
Kelvin Mai <kelvin.mai002@gmail.com>
2019-06-02 19:18:45 -0400

    Added user read and writes

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,17 +1,28 @@
 package models
 
 import play.api.libs.json.{Json, Writes}
+import reactivemongo.bson.{BSONObjectID, BSONDocument, BSONDocumentReader, BSONDocumentWriter}
+import reactivemongo.play.json._
 
 case class User(
-  _id: String,
+  _id: Option[BSONObjectID],
   username: String, 
   password: String)
 
 object User {
-  implicit val userWrites: Writes[User] = Writes { user =>
-    Json.obj(
-      "id" -> user._id,
+ implicit object UserWriter extends BSONDocumentWriter[User]{
+    def write(user:User):BSONDocument = BSONDocument(
+      "_id" -> user._id.getOrElse(BSONObjectID.generate),
       "username" -> user.username,
+      "password" -> user.password
     )
   }
+
+  implicit object UserReader extends BSONDocumentReader[User]{
+    def read(doc:BSONDocument):User = User(
+      doc.getAs[BSONObjectID]("_id"),
+      doc.getAs[String]("username").getOrElse(""),
+      doc.getAs[String]("passwoord").getOrElse("")
+    )
+  } 
 }
